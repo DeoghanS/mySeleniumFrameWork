d81c2d5be079de1b673d6384052df6cffa5278fc
Deoghan <deoghans24@gmail.com>
2024-05-16 16:34:28 +0200

    Phase 2 change

--- SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/tests/testGit.java
+++ SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/tests/testGit.java
@@ -6,13 +6,6 @@ public class testGit {
 @Test
 public void helloMsg()
 {
-	System.out.println("This is to test devtest branch vs master");
-;
-	
-	
-	System.out.println("Change 1 made by second user");
-	System.out.println("Change 1 made by second user");
-	System.out.println("Change 1 made by second user");
 	
 	System.out.println("This is a phase 2 change");
 	
