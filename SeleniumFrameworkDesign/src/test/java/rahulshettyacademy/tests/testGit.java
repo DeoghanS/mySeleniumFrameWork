32f2411464792c86f43b64a80170aa30bf2aa837
Deoghan <deoghans24@gmail.com>
2024-05-16 10:27:29 +0200

    My first commit

---
+++ SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/tests/testGit.java
@@ -0,0 +1,14 @@
+package rahulshettyacademy.tests;
+import org.testng.annotations.*;
+
+public class testGit {
+	
+@Test
+public void helloMsg()
+{
+	System.out.println("Change 2");
+	System.out.println("Change 2");
+	System.out.println("Change 2");
+}
+
+}
